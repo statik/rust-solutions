e2c11fa3c6624358062a707047c6ce1923ec9e2b
Elliot Murphy <statik@users.noreply.github.com>
2022-11-28 22:38:49 -0500

    completed chapter 3
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -40,13 +42,45 @@ pub fn get_args() -> MyResult<Config> {
         .get_matches();
 
     Ok(Config {
-        files: matches.values_of_lossy("text").unwrap(),
+        files: matches.values_of_lossy("files").unwrap(),
         number_lines: matches.is_present("number"),
         number_nonblank_lines: matches.is_present("number_nonblank"),
     })
 }
 
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+fn print(config: &Config, file: Box<dyn BufRead>) -> MyResult<()> {
+    let mut last_num = 0;
+    for (i, line) in file.lines().enumerate() {
+        let line = line?;
+        if config.number_lines {
+            println!("{:>6}\t{}", i + 1, line);
+        } else if config.number_nonblank_lines {
+            if !line.is_empty() {
+                last_num += 1;
+                println!("{:>6}\t{}", last_num, line);
+            } else {
+                println!();
+            }
+        } else {
+            println!("{}", line);
+        }
+    }
+    Ok(())
+}
+
 pub fn run(config: Config) -> MyResult<()> {
-    dbg!(config);
+    for filename in &config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(file) => print(&config, file)?,
+        }
+    }
     Ok(())
 }
